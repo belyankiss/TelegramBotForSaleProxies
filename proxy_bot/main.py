7cf4f760c4dfbaa3fe3cf327d672d6288984d6ab
belyankiss <belyankiss@gmail.com>
2024-10-19 14:19:41 +0300

    Дефолтная настройка бота.

---
+++ proxy_bot/main.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+import sys
+
+from proxy_bot.program_imports import bot, dp
+
+
+async def main() -> None:
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Bot stopped")
+
